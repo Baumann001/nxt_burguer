7a6b7243f8a1f265ff2be021e050add912baf45f
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2026-08-20 21:26:30 -0300

    add- tabela gerenciar unidades

--- adm_gerenciar.php
+++ adm_gerenciar.php
@@ -1,15 +1,45 @@
 <?php
 include "db.php";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$sql = "SELECT * FROM unidade";
+$result = $conn->query($sql);
 
-    $stmt_select = $conn->prepare("SELECT * FROM unidade");
-    $stmt_select->execute();
+    if ($result->num_rows > 0){
 
-    $result = $stmt_select->get_result();
+        echo " <h2>unidades:</h2> ";
+        echo "<table border = '1' cellpadding = '8' cellspacing = '0'>
+        
+        <tr>       
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Descrição</th>
+        </tr>";
+    
+    
+   while ($row = $result->fetch_assoc()){
 
-    while ($unidade = $result->fetch_assoc()) {
-        echo $unidade['nome'];
+    echo "<tr>
+
+    <td>{$row['id']}</td>
+    <td>{$row['nome']}</td>
+    <td>{$row['descricao']}</td>
+
+    </tr>";
+
+   }
+
+ echo "</table>";
+
+    }else{
+    echo "<p>Nenhuma unidade existe </p>";
     }
-}
+
+$conn->close();
+
+  
 ?>
+
+
+
+
+
