c7f0e355c6e9401477554d930bff1b6b60012376
Baumann001 <gustavo_baumann@estudante.sesisenai.org.br>
2026-08-20 21:48:36 -0300

    add- select funcionario

--- adm_gerenciar.php
+++ adm_gerenciar.php
@@ -34,7 +34,60 @@ $result = $conn->query($sql);
     echo "<p>Nenhuma unidade existe </p>";
     }
 
+
+
+
+
+$sql = "SELECT * FROM funcionario";
+$result = $conn->query($sql);
+
+    if ($result->num_rows > 0){
+
+        echo " <h2>Funcionários:</h2> ";
+        echo "<table border = '1' cellpadding = '8' cellspacing = '0'>
+        
+        <tr>       
+        
+        <th>Nome</th>
+        <th>Idade</th>
+        <th>CPF</th>
+        <th>Telefone</th>
+        <th>Cargo</th>
+    
+        </tr>";
+    
+    
+   while ($row = $result->fetch_assoc()){
+
+    echo "<tr>
+
+    
+    <td>{$row['nome']}</td>
+    <td>{$row['idade']}</td>
+    <td>{$row['cpf']}</td>
+    <td>{$row['telefone']}</td>
+    <td>{$row['cargo']}</td>
+
+    </tr>";
+
+   }
+
+ echo "</table>";
+
+    }else{
+    echo "<p>Nenhum funcionário existe existe </p>";
+    }
+
 $conn->close();
+
+  
+
+
+
+
+
+
+
 
   
 ?>
